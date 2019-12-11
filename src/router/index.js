81c4110d5d4f76166501ccfeb7af0bd3b4ce98a7
Donggyu Kim <kmdngyu@gmail.com>
2019-12-11 16:09:55 +0900

    fix: oauth2 redirect router change

--- src/router/index.js
+++ src/router/index.js
@@ -18,8 +18,8 @@ const routes = [
     component: () => import(/* webpackChunkName: "about" */ '../views/Users.vue')
   },
   {
-    path: '/github/callback',
-    name: 'github-callback',
+    path: '/oauth2/redirect',
+    name: 'oauth2/redirect',
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
