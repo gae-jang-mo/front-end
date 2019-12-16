c389ce97f7168e4627f1ebe09eb532103f5828c8
JunHoPark <pjh374968@gmail.com>
2019-12-16 16:52:39 +0900

    refactor: change dist folder name

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,6 @@
 module.exports = {
     outputDir: process.env.NODE_ENV === 'production'
-        ? '../app/src/main/resources/static'
+        ? '../app-server/src/main/resources/static'
         : './dist',
     css: {
         loaderOptions: {
